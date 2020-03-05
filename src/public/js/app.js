8d2db15b2d9b2511f868bfdf0ff96cdbe4d34270
fjuribe.14 <fjuribe.14@gmail.com>
2020-03-05 11:17:52 -0400

    app.js modify

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,7 +1,9 @@
 const main = document.querySelector(".main-container"),
   navbar = document.querySelector(".navbar"),
   sidenav = document.querySelector(".sidenav"),
-  messages = document.querySelector(".alert");
+  messages = document.querySelector(".alert"),
+  footer = document.querySelector('footer');
 
 main.style.marginTop = navbar.clientHeight + "px";
+main.style.marginBottom = footer.clientHeight + "px";
 messages.style.marginTop = navbar.clientHeight + "px";
